7eac49ea015f7b73b27c079a7b7b5ebe0b1f6ae3
Natalie Lampa <nlampa@umich.edu>
2020-07-21 19:35:42 -0500

    upfiles

---
+++ One For All 1.1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  One For All 1.1
+//
+//  Created by Apple on 7/21/20.
+//  Copyright © 2020 Jorge Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    //-------------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //------------------------------------- [OVERIDE]
+
+
+    
+    
+    
+    
+    
+    
+}// [V.C END]
+
